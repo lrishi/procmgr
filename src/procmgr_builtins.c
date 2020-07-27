602c8c7fe3f7694fe7661feb5051516c298f719c
Lovel Rishi <lovelworks@gmail.com>
2020-07-26 21:21:41 -0400

    Added functional support for piping output of forked process to /usr/sbin/more, now make prod ready

--- src/procmgr_builtins.c
+++ src/procmgr_builtins.c
@@ -7,23 +7,19 @@
 void
 procmgr_builtins_handle (char *cmd)
 {
-    pid_t pid;
     if (strcmp(cmd, "show process") == 0) {
         printf("\n");
         char *args[] = {
             "aux",
         };
-        procmgr_process_create("/usr/bin/ps", 1, args, &pid);
-        waitpid(pid, NULL, 0);
-        return;
+        procmgr_process_create("/usr/bin/ps", 1, args, NULL);
     }
     if (strcmp(cmd, "show kernel") == 0) {
         printf("\n");
         char *args[] = {
             "-a",
         };
-        procmgr_process_create("/usr/bin/uname", 1, args, &pid);
-        waitpid(pid, NULL, 0);
-        return;
+        procmgr_process_create("/usr/bin/uname", 1, args, NULL);
     }
+    while (wait(NULL) >= 0);
 }
--- src/procmgr_proc.c
+++ src/procmgr_proc.c
@@ -1,12 +1,16 @@
 #include <procmgr_types.h>
 #include <procmgr_error.h>
 #include <procmgr_proc.h>
+#include <procmgr_debug.h>
 
 #include <pty.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+
+#define READ_END 0
+#define WRITE_END 1
 
 pmerr
 procmgr_process_create (char    *executable,
@@ -15,13 +19,37 @@ procmgr_process_create (char    *executable,
                         pid_t   *pid)
 {
     pmerr   pe          = PM_EOK;
-    pid_t   create_pid  = fork();
     char  **arguments   = NULL;
 
+    pid_t   create_pid;
+    int pipefd[2];
+    
+    if (pipe(pipefd) == -1) {
+        PROCMGR_LOG_ERROR("Failed to create named pipe");
+        return errno;
+    }
+    
+    if (fork() == 0) {
+        
+        if (dup2(pipefd[READ_END], STDIN_FILENO) == -1) {
+            PROCMGR_LOG_ERROR("Failed to duplicate named pipe");
+            return errno;
+        }
 
+        close(pipefd[READ_END]);
+        close(pipefd[WRITE_END]);
+        
+        char *args[] = {
+            "/usr/bin/more",
+            NULL
+        };
+
+        execvp("/usr/bin/more", args);
+    }
+
+    create_pid = fork();
     if (create_pid == 0) {
         /* New Process */
-        
         arguments = calloc(argc + 2, sizeof(char*));
 
         if (arguments == NULL) {
@@ -34,6 +62,15 @@ procmgr_process_create (char    *executable,
         for (int i = 0; i < argc; i++) {
             arguments[i + 1] = strdup(args[i]);
         }
+        
+        if (dup2(pipefd[WRITE_END], STDOUT_FILENO) == -1) {
+            PROCMGR_LOG_ERROR("Failed to duplicate named pipe");
+            return -1;
+        }
+
+        // Close the pipe now that we've duplicated it
+        close(pipefd[READ_END]);
+        close(pipefd[WRITE_END]);
 
         if (execvp(executable, arguments) == -1) {
             /* Notify error (TODO) then exit */
@@ -44,15 +81,13 @@ procmgr_process_create (char    *executable,
         exit(ENOTSUP);
 
     } else {
+        close(pipefd[READ_END]);
+        close(pipefd[WRITE_END]);
         /* Parent process */
-        
-        if (errno != 0) {
-            pe = errno;
-        } else {
-            if (pid != NULL) {
-                *pid = create_pid;
-            }
+        if (pid != NULL) {
+            *pid = create_pid;
         }
+
     }
     
     return pe;
